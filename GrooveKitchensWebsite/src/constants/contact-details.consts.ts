7b0b58c24389d2a48fa3632de58d9a56dafec15e
Keeghan Campbell <keeghancampbell@gmail.com>
2026-08-26 20:54:17 +1200

    Update Groove Kitchens phone number

--- GrooveKitchensWebsite/src/constants/contact-details.consts.ts
+++ GrooveKitchensWebsite/src/constants/contact-details.consts.ts
@@ -2,7 +2,7 @@ import { TContactDetails } from "../contactus/contact-details.type";
 
 export const contactDetails : TContactDetails = {
     email: 'groovekitchens@gmail.com',
-    mobileNumber: '+64 27 283 1088',
+    mobileNumber: '021 397 863',
 } as const satisfies TContactDetails;
 
-export const mobileOneWord = contactDetails.mobileNumber.replace(" ", "");
+export const mobileOneWord = contactDetails.mobileNumber.replace(/\s/g, "");
