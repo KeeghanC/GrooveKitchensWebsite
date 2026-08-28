fa194d2c9fe537414b3c3b04383653cca7df84bb
Keeghan Campbell <keeghancampbell@gmail.com>
2026-08-29 09:42:50 +1200

    Refine navigation call link and favicon

--- src/constants/nav-content.const.ts
+++ src/constants/nav-content.const.ts
@@ -13,7 +13,4 @@ export const NAV_CONTENT = {
     { label: 'Contact', url: '/contact' },
     { label: 'Testimonials', url: '/testimonials' },
   ],
-  cta: {
-    label: 'Request a Quote',
-  },
 } as const;
